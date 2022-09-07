0038017068b31172c843b1836eac9b5ffac2a160
Aleksei Bodrov <bellied.strut_0v@icloud.com>
2022-09-07 12:29:59 +0300

    07.09.2022

---
+++ IMusic/Helpers/CMTime + Extension.swift
@@ -0,0 +1,16 @@
+
+import Foundation
+import AVKit
+
+extension CMTime {
+    
+    func toDisplayString() -> String {
+        guard !CMTimeGetSeconds(self).isNaN else { return "" }
+        let totalSecond = Int(CMTimeGetSeconds(self))
+        let seconds = totalSecond % 60
+        let minutes = totalSecond / 60
+        let timeFormatString = String(format: "%02d:%02d", minutes, seconds)
+        return timeFormatString
+    }
+    
+}
--- IMusic/Services/NetworkService.swift
+++ IMusic/Services/NetworkService.swift
@@ -1,10 +1,4 @@
-//
-//  NetworkService.swift
-//  IMusic
-//
-//  Created by Алексей Пархоменко on 12/08/2019.
-//  Copyright © 2019 Алексей Пархоменко. All rights reserved.
-//
+
 
 import UIKit
 import Alamofire
@@ -31,7 +25,7 @@ class NetworkService {
             let decoder = JSONDecoder()
             do {
                 let objects = try decoder.decode(SearchResponse.self, from: data)
-                print("objects: ", objects)
+//                print("objects: ", objects)
                 competion(objects)
                 
             } catch let jsonError {
